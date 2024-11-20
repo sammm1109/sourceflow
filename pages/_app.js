3fe30d28904fa50a9f1e42ca3c4ffc67b3eb6bdb
samantha-1109 <samantha@skycity.gg>
2024-11-20 14:04:16 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // pages/_app.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.scss";
-import Layout from "../components/Layout";
+import Layout from "../components/layout.js";
 
 function MyApp({ Component, pageProps }) {
   return (
